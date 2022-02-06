5a082bc0b5457666404c6ad0a3f04d8a1e967b68
Kristen <kristenwood123@gmail.com>
2022-02-06 10:30:54 -0800

    passport

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -37,6 +37,11 @@ app.use(express.urlencoded({ extended: false }));
 
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(cookieParser("12345-67890-09876-54321"));
 
@@ -54,19 +59,13 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+  console.log(req.user);
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      return next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
